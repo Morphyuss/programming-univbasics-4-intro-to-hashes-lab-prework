b0e1262468aabafe99b53b445a8fb6b3fdf79d54
James Stamps Jr <jstampshere@gmail.com>
2020-04-17 14:22:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def update_counting_hash(hash, key)
   if hash.value?key
     hash[key] +=1
   end
-  hash
+  hash = 1
 end
 # given a hash an a key as parameters, return an updated hash
 # if the provided key is not present in the hash, add it and assign it to the value of 1
