037439b6633359eef00ff548da8b4a6159abfa81
James Stamps Jr <jstampshere@gmail.com>
2020-04-17 13:22:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,12 +23,12 @@ end
 
 def my_hash_creator(key, value)
   {
-    key=> value
+    key => value
   }# return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  read_from_hash[key]# return the correct value using the hash and key parameters
 end
 
 def update_counting_hash(hash, key)
